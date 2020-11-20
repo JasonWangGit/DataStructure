cf2d96b99195e01875d1d539ec46faf22dde07fc
wangj <wangjiachuan@vip.163.com>
2020-11-20 09:19:16 +0800

    Version 3 for QuickSort.

--- src/Sorting/QuickSort_v3.java
+++ src/Sorting/QuickSort_v3.java
@@ -3,7 +3,7 @@ package Sorting;
 public class QuickSort_v3 {
 	public static int[] sort(int[] nums) {
 	    recur(nums, 0, nums.length - 1);
-		return nums;
+	    return nums;
 	}
 		
 	public static void recur(int[] nums, int left, int right) {
@@ -16,10 +16,10 @@ public class QuickSort_v3 {
 
 	public static int partition(int[] nums, int left, int right) {
 	    if(left == right) return left;
-		int pivotal = nums[left];
+	    int pivotal = nums[left];
 	    int i = left + 1, j = right;
 	    while(true) {
-	        while(nums[i] <= pivotal && i < right) i++;
+	        while(nums[i] < pivotal && i < right) i++;
 	        while(nums[j] > pivotal && j > left) j--;
 	        if(i < j) swap(nums, i, j);
 	        else break;
