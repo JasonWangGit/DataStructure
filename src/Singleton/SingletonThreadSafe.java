7c36070fd48a042de741c3d2c79bfaa2d70be42e
wangj <wangjiachuan@vip.163.com>
2020-11-20 09:51:58 +0800

    Version 1 for SingletonThreadSafe.

---
+++ src/Singleton/SingletonThreadSafe.java
@@ -0,0 +1,18 @@
+package Singleton;
+
+public class SingletonThreadSafe {
+	private static volatile SingletonThreadSafe singletonInstance;
+    
+    private SingletonThreadSafe() {}
+    
+    public static SingletonThreadSafe getInstance() {
+        if(singletonInstance == null) {
+            synchronized(SingletonThreadSafe.class) {
+                if(singletonInstance == null) {
+                    singletonInstance = new SingletonThreadSafe();
+                }
+            }
+        }
+        return singletonInstance;
+    }
+}
